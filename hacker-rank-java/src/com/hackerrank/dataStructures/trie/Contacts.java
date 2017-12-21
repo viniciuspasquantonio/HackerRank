cf40749928d6dd4c971ebf27ad049709bb6c973e
vinicius pasquantonio <vinicius pasquantonio@walmart.com>
2017-12-21 14:55:56 -0200

    contacts trie

--- hacker-rank-java/src/com/hackerrank/dataStructures/trie/Contacts.java
+++ hacker-rank-java/src/com/hackerrank/dataStructures/trie/Contacts.java
@@ -7,44 +7,78 @@ public class Contacts {
 	public static void main(String[] args) {
 		final Scanner in = new Scanner(System.in);
 		int qtd = in.nextInt();
-		String[] inputs = new String[qtd];
-		Stack<String> command = new Stack<>();
-		Stack<String> words = new Stack<>();
+		Trie trie = new Trie();
 		for (int i = 0; i < qtd; i++) {
-			command.push(in.next());
-			words.push(in.next());
+			String command = in.next();
+			String word = in.next();
+			if("add".equals(command)){
+				trie.insert(word);
+			}else if ("find".equals(command)){
+				System.out.println(trie.countOcurrences(word));
+			}
 		}
 	}
 	
 	static class Node {
-		private int letter;
-		private boolean finalLetter;
-		private int[] nextLetters;
-		public Node(int letter) {
-			this.letter = letter;
+		public Node() {
+			this.childreen = new Node[26];
 		}
-		public int getLetter() {
-			return letter;
+		private int data;
+		private String letra;
+		private boolean leaf;
+		private Node[] childreen;
+		private int quantity;
+		
+		public Node getChild(int index) {
+			return childreen[index];
 		}
-		public boolean isFinalLetter() {
-			return finalLetter;
-		}
-		public int[] getNextLetters() {
-			return nextLetters;
-		}
-		public void setLetter(int letter) {
-			this.letter = letter;
-		}
-		public void setFinalLetter(boolean finalLetter) {
-			this.finalLetter = finalLetter;
-		}
-		public void setNextLetters(int[] nextLetters) {
-			this.nextLetters = nextLetters;
+		public void setChild(int index, int data) {
+			Node node = new Node();
+			node.data = data;
+			childreen[index] = node;
+			
 		}
 	}
 
-	static class Triee{
+	static class Trie{
 		private Node root;
+
+		public Trie() {
+			this.root = new Node();
+		}
+		public int countOcurrences(String word) {
+			Node tempNode = root;
+			int ascIndex;
+			int ocurrences =0;
+			for (int i = 0; i < word.length(); i++) {
+				ascIndex = word.charAt(i)-'a';
+				if(tempNode.getChild(ascIndex) == null){
+					return ocurrences;
+				}
+				tempNode = tempNode.getChild(ascIndex);
+					
+			}
+			ocurrences = tempNode.quantity;
+			return ocurrences;
+		}
+		public void insert(String word) {
+			Node tempNode = root;
+			int ascIndex;
+			for (int i = 0; i < word.length(); i++) {
+				int data = word.charAt(i);
+				ascIndex = word.charAt(i)-'a';
+				if(tempNode.getChild(ascIndex) == null){
+					tempNode.setChild(ascIndex,data);
+				}
+				
+				tempNode = tempNode.getChild(ascIndex);
+				tempNode.quantity++;
+					
+			}
+			
+			tempNode.leaf = true;
+			
+		}
 		
 	}
 	
